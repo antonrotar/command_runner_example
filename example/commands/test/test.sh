e5d38267e0e4cb2f06026057a8a765c412c35277
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 11:55:37 +0100

    Rename commands directory

--- example/commands/test/test.sh
+++ example/commands/test/test.sh
@@ -2,12 +2,12 @@
 
 script_directory="$(dirname "$0")"
 
-if ! "$script_directory/../passing_test.sh"; then
+if ! "$script_directory/../passing_command.sh"; then
   echo "Test should pass but fails."
   exit 1
 fi
 
-if "$script_directory/../failing_test.sh"; then
+if "$script_directory/../failing_command.sh"; then
   echo "Test should fail but passes."
   exit 1
 fi
