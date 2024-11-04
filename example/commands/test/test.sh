0b5e58cf9f481fc2750125578a9007a88526d3c3
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 12:56:45 +0100

    One name per concept

--- example/commands/test/test.sh
+++ example/commands/test/test.sh
@@ -1,14 +1,14 @@
-# This test verifies the execution of test scripts.
+# This test verifies the execution of commands.
 
 script_directory="$(dirname "$0")"
 
 if ! "$script_directory/../passing_command.sh"; then
-  echo "Test should pass but fails."
+  echo "Command should pass but fails."
   exit 1
 fi
 
 if "$script_directory/../failing_command.sh"; then
-  echo "Test should fail but passes."
+  echo "Command should fail but passes."
   exit 1
 fi
 
