05248f67ef8db82ec542fbbe239e23e091581ffa
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 12:56:45 +0100

    One name per concept

--- example/commands/failing_command.sh
+++ example/commands/failing_command.sh
@@ -1,6 +1,6 @@
 # Basic script returning a negative value as a substitute
 # for whatever command the command runner will be executing.
 
-echo "This is a failing test."
+echo "This is a failing command."
 
 exit 1
--- example/commands/passing_command.sh
+++ example/commands/passing_command.sh
@@ -1,6 +1,6 @@
 # Basic script returning a positive value as a substitute
 # for whatever command the command runner will be executing.
 
-echo "This is a passing test."
+echo "This is a passing command."
 
 exit 0
