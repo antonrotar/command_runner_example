b0618844cb4b6bd743966f637bd935e2a1b38957
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 12:22:53 +0100

    Add example with expectation

---
+++ example/03_example_with_specific_expectation.sh
@@ -0,0 +1,10 @@
+# This example adds and runs two passing commands.
+# The overall return value will be positive.
+
+script_directory="$(dirname "$0")"
+source "$script_directory/../command_runner/command_runner.sh"
+
+command_runner_add_specific ./$script_directory/commands/failing_command.sh 1
+command_runner_add ./$script_directory/commands/passing_command.sh
+
+command_runner_run
--- example/test/test.sh
+++ example/test/test.sh
@@ -12,4 +12,9 @@ if "$script_directory/../02_failing_example.sh"; then
   exit 1
 fi
 
+if ! "$script_directory/../03_example_with_specific_expectation.sh"; then
+  echo "Example should fail but passes."
+  exit 1
+fi
+
 echo "All tests passed."
