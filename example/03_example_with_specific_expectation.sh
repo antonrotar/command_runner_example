379d034ec2f50420b92bd0466ae6b2032b6f2566
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 12:30:15 +0100

    Simplify example

--- example/03_example_with_specific_expectation.sh
+++ example/03_example_with_specific_expectation.sh
@@ -5,6 +5,5 @@ script_directory="$(dirname "$0")"
 source "$script_directory/../command_runner/command_runner.sh"
 
 command_runner_add_specific ./$script_directory/commands/failing_command.sh 1
-command_runner_add ./$script_directory/commands/passing_command.sh
 
 command_runner_run
