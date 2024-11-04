7e47ad84f6e84fad57ab424262ef5c19a5891442
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 11:14:28 +0100

    Add tests to execute from command runner

---
+++ example/test/test/test.sh
@@ -0,0 +1,11 @@
+script_directory="$(dirname "$0")"
+
+if ! "$script_directory/../passing_test.sh"; then
+  echo "Test should pass but fails."
+  exit 1
+fi
+
+if "$script_directory/../failing_test.sh"; then
+  echo "Test should fail but passes."
+  exit 1
+fi
