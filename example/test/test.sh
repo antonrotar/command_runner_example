2e923ecfc2b036d120135c201ec8c96f51e6c6d3
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-04 12:05:10 +0100

    Add examples test

---
+++ example/test/test.sh
@@ -0,0 +1,8 @@
+# This test verifies the assumptions from the example execution.
+
+script_directory="$(dirname "$0")"
+
+if ! "$script_directory/../01_simple_example.sh"; then
+  echo "Example should pass but fails."
+  exit 1
+fi
