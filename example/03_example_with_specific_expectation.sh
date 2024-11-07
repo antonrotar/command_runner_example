72a83074d9b8338e3ab4d0197044a116f3e1c3c9
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-07 13:21:24 +0100

    Bump command runner version

--- example/03_example_with_specific_expectation.sh
+++ example/03_example_with_specific_expectation.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 script_directory="$(dirname "$0")"
 source "$script_directory/../command_runner/command_runner.sh"
 
-command_runner_add_specific ./$script_directory/commands/failing_command.sh 1
+command_runner_add_with_expectation ./$script_directory/commands/failing_command.sh 1
 
 command_runner_run
