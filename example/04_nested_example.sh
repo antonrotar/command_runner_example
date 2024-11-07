dc05ba978f0d4c306385eebfad93b5ea890fcf28
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-07 13:21:24 +0100

    Bump command runner version

--- example/04_nested_example.sh
+++ example/04_nested_example.sh
@@ -8,6 +8,6 @@ source "$script_directory/../command_runner/command_runner.sh"
 
 command_runner_add ./$script_directory/01_simple_example.sh
 command_runner_add ./$script_directory/02_failing_example.sh
-command_runner_add_specific ./$script_directory/02_failing_example.sh 1
+command_runner_add_with_expectation ./$script_directory/02_failing_example.sh 1
 
 command_runner_run
